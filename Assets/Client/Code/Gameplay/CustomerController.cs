af04fbb0f56b5ac0a78615dac469a4ca90698ce1
PotokaAlex <nevskial686@gmail.com>
2025-07-02 16:47:49 +0300

    Ai navigation

---
+++ Assets/Client/Code/Gameplay/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Client.Code.Gameplay
+{
+    public class CustomerController : MonoBehaviour
+    {
+        public Transform Target;
+        public NavMeshAgent NavMeshAgent;
+
+        private void Update() => NavMeshAgent.SetDestination(Target.position);
+    }
+}
